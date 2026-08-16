da7e556525f381529f58fd371a05b5a0f8bf7b3a
Rakha Avilla <azmyervawanrakhaavilla@gmail.com>
2026-08-16 17:17:02 +0700

    Runing Fiber And Schema Databases

--- cmd/server/main.go
+++ cmd/server/main.go
@@ -4,9 +4,23 @@ import (
 	"log"
 
 	"github.com/gofiber/fiber/v3"
+
+	"github.com/Rakaa503/AviGo/internal/config"
+	"github.com/Rakaa503/AviGo/internal/database"
 )
 
 func main() {
+	cfg := config.Load()
+
+	db := database.Connect(cfg.DatabaseURL)
+
+	if err := database.Ping(db); err != nil {
+		log.Fatalf(
+			"database ping failed: %v",
+			err,
+		)
+	}
+
 	app := fiber.New()
 
 	app.Get("/", func(c fiber.Ctx) error {
@@ -22,9 +36,13 @@ func main() {
 		})
 	})
 
-	log.Println("AVIGO API running on :8080")
+	log.Printf(
+		"%s API running on :%s",
+		cfg.AppName,
+		cfg.AppPort,
+	)
 
-	if err := app.Listen(":8080"); err != nil {
+	if err := app.Listen(":" + cfg.AppPort); err != nil {
 		log.Fatal(err)
 	}
 }
